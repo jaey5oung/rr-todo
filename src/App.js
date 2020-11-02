2190139d106472c801017cf2f6f00e25e37d1b76
jy <jaey5oung@naver.com>
2020-11-02 22:08:41 +0900

    리덕스모듈숙지

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import react, { useState } from "react"
+
 
 function App() {
+  const [good,setGood] = useState("") 
+
+  const onNote =(e)=>{
+    setGood(e.target.value)
+  }
+
+  const onClick =()=>{
+    console.log(good);
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input value={good} type="text" onChange={onNote} />
+      <button onClick={onClick}></button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
